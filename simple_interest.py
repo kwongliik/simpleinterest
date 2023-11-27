64a24f3782aac84573792b42616722fbc6fd0be9
kwongliik <46083661+kwongliik@users.noreply.github.com>
2023-11-27 23:02:06 +0800

    Update simple_interest.py create question

--- simple_interest.py
+++ simple_interest.py
@@ -1,17 +1,22 @@
-def calculate_simple_interest(P, r, t):
-    simple_interest = P * (r/100) * t
-    return simple_interest
+# A program that can calculate simple interest
 
+# calculate_simple_interest() is a function that will return simple_interest as a variable 
+def calculate_simple_interest(P, r, t):
+    ..................................
+    ..................................
+
+# get_user_input() is a function that will return principal, rate and time values
 def get_user_input():
-    principal = float(input("Key in the principal: "))
-    rate = float(input("Key in the interest rate per annum: "))
-    time = int(input("Key in number of years: "))
-    return principal, rate, time
+    principal = float(input("Key in the principal: "))          # Don't change the code!
+    rate = float(input("Key in the interest rate per annum: ")) # Don't change the code!
+    time = int(input("Key in number of years: "))               # Don't change the code!
+    .................................
 
 def main():
-    principal, rate, time = get_user_input() 
-    interest_earned = calculate_simple_interest(principal, rate, time)
+    principal, rate, time = ...........................         # Call the function to get user input
+    interest_earned = .................................         # Call the function to calculate simple interest
     print(f"Interest earned is {interest_earned:.2f}")
 
+# Don't change the code below!
 if __name__ == "__main__":
     main()
